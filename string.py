2aa953fb5b74fc9597b67f359b2f107179e67af1
Sonam Vajpayee <saraamitdube@gmail.com>
2023-08-20 19:18:50 +0300

    learing strings in  py

---
+++ string.py
@@ -0,0 +1,42 @@
+# poem = 'Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\t Up above the world so high, \n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are'
+# print(poem)
+      
+      
+# first_line = ('Twinkle, twinkle, little star')
+# print(first_line)
+# second_line =('    How I wonder you are!')    
+# print(second_line) 
+# third_line = ('     Up above the world so high,')
+# print(third_line)
+# from datetime import datetime
+
+# current_date = datetime.now()
+# print(current_date)
+# print(current_date.strftime('%d/%m/%y '))
+
+# import math
+
+# radius = input('Enter radius of circle ' )
+
+# area_of_circle = math.pi*float(radius)*float(radius)
+# print (area_of_circle)
+
+# # write user name with space "" reverse 
+# fname =input ('enter your first name ')
+# Lname =input ('enter your last name ')
+# print (Lname + " " + fname)
+
+# values = input('enter items :')
+# list = values.split(',')
+# print('list: ', list)
+
+# file = input('write file name :')
+# f_extns = file.split(',')
+# print('file extension:'+ repr(f_extns[-1]))
+
+file_name = input ('enter file name ')
+file_name_list = file_name.split('.')
+print(file_name_list[file_name_list.count - 1])
+
+
+
